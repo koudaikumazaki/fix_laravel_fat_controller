6f0feb26851f87182ca2275d0ab37ff69d212d86
koudaikumazaki <kk1974340@gmail.com>
2022-02-13 16:08:40 +0900

    separate: useCase

--- src/app/Http/Controllers/Bookmarks/BookmarkController.php
+++ src/app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -22,6 +22,8 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use App\Bookmark\UseCase\ShowBookmarkListPageUseCase;
+use App\Bookmark\UseCase\CreateBookmarkUseCase;
+use App\Http\Requests\CreateBookmarkRequest;
 
 class BookmarkController extends Controller
 {
@@ -123,43 +125,13 @@ class BookmarkController extends Controller
      * @param Request $request
      * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function create(Request $request)
+    public function create(CreateBookmarkRequest $request)
     {
-        if (Auth::guest()) {
-            // @note ここの処理はユーザープロフィールでも使われている
-            return redirect('/login');
-        }
+      $useCase = new CreateBookmarkUseCase();
+      $useCase->handle($request->url, $request->category, $request->comment);
 
-        Validator::make($request->all(), [
-            'url' => 'required|string|url',
-            'comment' => 'required|string|min:10|max:1000',
-            'category' => 'required|integer|exists:bookmark_categories,id',
-        ])->validate();
-
-        // 下記のサービスでも同様のことが実現できる
-        // @see https://www.linkpreview.net/
-        $previewClient = new Client($request->url);
-        try {
-            $preview = $previewClient->getPreview('general')->toArray();
-
-            $model = new Bookmark();
-            $model->url = $request->url;
-            $model->category_id = $request->category;
-            $model->user_id = Auth::id();
-            $model->comment = $request->comment;
-            $model->page_title = $preview['title'];
-            $model->page_description = $preview['description'];
-            $model->page_thumbnail_url = $preview['cover'];
-            $model->save();
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            throw ValidationException::withMessages([
-                'url' => 'URLが存在しない等の理由で読み込めませんでした。変更して再度投稿してください'
-            ]);
-        }
-
-        // 暫定的に成功時は一覧ページへ
-        return redirect('/bookmarks', 302);
+      // 暫定的に成功時は一覧ページへ
+      return redirect('/bookmarks', 302);
     }
 
     /**
